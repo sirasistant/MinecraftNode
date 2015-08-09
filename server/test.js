781e4c7a1fe70d497bb4a38568ea1ccd37e04d76
Alvaro Rodriguez Villalba <alvaro.r.villalba@outlook.com>
2015-08-09 17:03:53 +0200

    Deleted file

--- server/test.js
+++
@@ -1,7 +0,0 @@
-var schedule = require('node-schedule');
-var cron = require('cron');
-var CronJob=cron.CronJob;
-
-new CronJob('0 * * * * *', function() {
-   console.log('The answer to life, the universe, and everything!');
-}, null, true, 'Europe/Madrid');
